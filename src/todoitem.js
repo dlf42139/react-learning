30c906717d85ecd5cfd178bf82e0e6fbbe36224f
kei_mac <dlf42139@163.com>
2019-10-19 03:00:37 +0800

    just try

---
+++ src/todoitem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ToDoItem extends Component {
+	render() {
+		return <div>hhh</div>
+	}
+}
+
+export default ToDoItem
---
+++ src/todolist.js
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from 'react'
+import ToDoItem from './todoitem'
+
+class toDoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      vlaueList: []
+    }
+  }
+  handleChande(e) {
+    // console.log('event',e.target);
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  addItem = () => {
+    let list = [...this.state.vlaueList, this.state.inputValue]
+    // list.push(this.state.inputValue)
+    this.setState({
+      vlaueList: list,
+      inputValue: ''
+    })
+  }
+  deleteItem = (idx) => {
+    let _list = [...this.state.vlaueList]
+    _list.splice(idx, 1)
+    this.setState({
+      vlaueList: _list
+    })
+  }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <label htmlFor="please">请输入：</label>
+          <input
+            id="please"
+            placeholder="hello react"
+            value={this.state.inputValue}
+            onChange={this.handleChande.bind(this)}></input>
+          <button onClick={this.addItem}>提交</button>
+        </div>
+        {
+          this.state.vlaueList.map((item, idx) => {
+            return <ToDoItem key={idx} value={item}/>
+            // return <li
+            //   key={idx}
+            //   onClick={() => {
+            //     this.deleteItem(idx)
+            //   }}
+            //   dangerouslySetInnerHTML={{ __html: item }}
+            // >
+            // </li>
+            
+          })
+        }
+      </Fragment>
+    )
+  }
+}
+export default toDoList
