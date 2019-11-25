3dfc5b7fc0e0708a600c100154b8d8a0850c73ae
kei_mac <dlf42139@163.com>
2019-11-25 21:01:04 +0800

    tochange

--- src/todolist.js
+++ src/todolist.js
@@ -1,58 +1,10 @@
-import React, { Component, Fragment } from 'react'
-import ToDoItem from './todoitem'
-
-class toDoList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      inputValue: '',
-      vlaueList: []
-    }
-  }
-  handleChande(e) {
-    // console.log('event',e.target);
-    this.setState({
-      inputValue: e.target.value
-    })
-  }
-  addItem = () => {
-    let list = [...this.state.vlaueList, this.state.inputValue]
-    // list.push(this.state.inputValue)
-    this.setState({
-      vlaueList: list,
-      inputValue: ''
-    })
-  }
-  deleteItem = (idx) => {
-    let _list = [...this.state.vlaueList]
-    _list.splice(idx, 1)
-    this.setState({
-      vlaueList: _list
-    })
-  }
+import React, { Component } from 'react'
+import { Input } from 'antd'
+class View extends Component {
   render() {
-    return (
-      <Fragment>
-        <div>
-          <label htmlFor="please">请输入：</label>
-          <input
-            id="please"
-            placeholder="hello react"
-            value={this.state.inputValue}
-            onChange={this.handleChande.bind(this)}></input>
-          <button onClick={this.addItem}>提交</button>
-        </div>
-        {
-          this.state.vlaueList.map((item, idx) => {
-            return <ToDoItem
-              key={idx}
-              idx={idx}
-              value={item}
-              deleteItem={this.deleteItem.bind(this)} />
-          })
-        }
-      </Fragment>
-    )
+    return <div>
+      <Input placeholder="hhh"/>
+    </div>
   }
 }
-export default toDoList
+export default View
