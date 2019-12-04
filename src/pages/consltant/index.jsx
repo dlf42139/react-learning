b3d9b3a294bb14e622bb2a142d8bfac5841d60d8
kei_mac <dlf42139@163.com>
2019-12-04 22:00:50 +0800

    tab change

--- src/pages/consltant/index.jsx
+++ src/pages/consltant/index.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './index.less'
 class View extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      current:0
+      current: 2,
+      content: [
+        {
+          name: '人群营销',
+          data: [999, 666]
+        },
+        {
+          name: '货品营销',
+          data: [999, 233]
+        },
+        {
+          name: '定向营销',
+          data: [999, 1]
+        },
+      ]
     }
   }
-  render(){
-    return(
-      <div>hh</div>
+  render() {
+    return (
+      <div className="container">
+        <div className="tab-head">
+          {
+            this.state.content.map((item, idx) => {
+              return (
+                <div
+                  className={idx === this.state.current ? "head active" : "head"}
+                  key={idx}
+                  onClick={() => { this.itemClick(idx) }}>
+                  <div>{item.name}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="tabcontent">
+          {
+            this.state.content.map((item, idx) => {
+              return (
+                <ul
+                  className={idx === this.state.current ? "active-content" : ''}
+                  key={idx}
+                >
+                  {
+                    item.data.map(dd => {
+                      return <li>{dd}</li>
+                    })
+                  }
+                </ul>
+              )
+            })
+          }
+        </div>
+      </div>
     )
+  }
+  itemClick = (idx) => {
+    this.setState({
+      current: idx
+    })
   }
 }
 
