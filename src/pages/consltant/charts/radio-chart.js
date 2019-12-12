aac6c862225adcbbd9e6692a8878f88580d224a5
kei_mac <dlf42139@163.com>
2019-12-12 22:05:13 +0800

    开始使用雷达图

---
+++ src/pages/consltant/charts/radio-chart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  G2,
+  Chart,
+  Geom,
+  Axis,
+  Tooltip,
+  Coord,
+  Label,
+  Legend,
+  View,
+  Guide,
+  Shape,
+  Facet,
+  Util
+} from 'bizcharts';
+class View extends React.Component{
+
+}
+export default View
